ddab91e805db71fc9ed92f78ce4c277c1eae63dd
Arun G <arunaakash675@gmail.com>
2025-12-06 09:05:25 +0530

    Implement Two Sum solution in TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,3,7,6} ;
+        int target = 9 ; 
+     
+       for(int i=0 ; i<arr.length ;i++){
+           for(int j=i+1 ; j<arr.length ; j++){
+               if(arr[i] + arr[j] == target){
+                 System.out.println(arr[i] + " " + arr[j]); 
+               }
+           }
+       }
+      
+    }
+}
