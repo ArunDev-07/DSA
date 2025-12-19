968a7bd8bad26313eecc0bc03623a01a43bebee5
Arun G <arunaakash675@gmail.com>
2025-12-19 07:20:48 +0530

    Enhance comments for clarity on logic
    Added comments to clarify the logic for finding the smallest letter greater than the target.

--- SmallestLetterGreaterthanTarget.java
+++ SmallestLetterGreaterthanTarget.java
@@ -16,3 +16,8 @@ class Solution {
         
     }
 }
+
+// in this problem we need to find the smallest element greater than the target same like a ceiling but in this we no need to find the same elemtent as the target .
+// because they asking that smallest element that greater than the target so == is not used if the target is found in the array we print the next value .
+// if the next value is not there we need to print the first element . using arr[start % arr.length] .if arr[start] is 3 means then the loop stops because the 2 indices are there.
+ // then start % arr.length 3 % 3 = 0 then it prints the first value if it is same it iterates with start = mid + 1 .
