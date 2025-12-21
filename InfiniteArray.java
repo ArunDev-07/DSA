e0bf84a6b9930df5a46477bfd4dc0cc654a3b120
Arun G <arunaakash675@gmail.com>
2025-12-21 10:12:01 +0530

    Enhance InfiniteArray.java with explanatory comments
    Added comments to explain the logic of finding the target in an infinite array.

--- InfiniteArray.java
+++ InfiniteArray.java
@@ -1,5 +1,3 @@
-package com.example.DSA_JAVA.BinarySearch;
-
 public class InfiniteArray {
     public static void main (String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16} ;
@@ -35,3 +33,8 @@ public class InfiniteArray {
         return -1 ;
     }
 }
+// in this problem we had infinte array so we didnt know the array length so we didnt have the end now we find the end first then we make the method.
+// in this we find the start and end that means we first take the start as  0 and end as 1. the while loop runs until the target is greater than or equal to the end.
+// newstart means start = 0 end = 1 then newstart =  end + 1 it starts from next iteration that means after the end why we didnt put start instead of newstart because we use start in the end also we need that old start.
+// then end  = end + (end - start + 1 ) * 2 that means 1 + (1 - 0 + 1) * 2 = 5 then start = 2 now it checks from 2 to 5 . first it takes 2 values then it takes 4 values then it takes 8 values like that it checks until the target>=end .
+// then we use that start and end in that normal binary search then we find the mid value.
