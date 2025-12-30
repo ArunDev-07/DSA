4164bdc6bf04ce7442c98b11645a235f6be0dcf7
Arun G <arunaakash675@gmail.com>
2025-12-30 20:07:51 +0530

    Enhance comments for swap method explanation
    Added comments explaining the swap method in the array.

--- SwapingArray.java
+++ SwapingArray.java
@@ -15,3 +15,4 @@ class Main {
 
 
 //in this problem we swap the values in the array using swap method in that we get the array and swapping indices , then we swap it using temp variable.
+//if we put without temp the answer will be the same because 3 = 3 then 3 = again 3 so we use temp as a variable .
